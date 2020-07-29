4d79fb38e2b44054449af5eec98153e6c9a5c01b
13242 <1324263842@qq.com>
2020-07-29 15:38:41 +0800

    默认

--- src/main/scala/com/amzport/mock/space/Mock1010.scala
+++ src/main/scala/com/amzport/mock/space/Mock1010.scala
@@ -1,18 +1,33 @@
 package com.amzport.mock.space
 
 import akka.actor.{ActorRefFactory, ActorSystem, ClassicActorSystemProvider}
-import akka.actor.typed.scaladsl.Behaviors
-import akka.http.scaladsl.model.ws.TextMessage
-import com.amzport.mock.MockSystem.system
-import com.amzport.mock.test.Test.outActor
+import com.amzport.mock.MockUser
 import pb.buyuwrapper.WrapperBuyuMessage
-object Mock1010 {
+
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext
+object Mock1010 extends App {
 
   case class BuYuCass(order: Int, buYuMess: WrapperBuyuMessage)
+  var buYuList = List.empty[BuYuCass]
+
   //捕鱼邀请消息
-  val buYuInvitePlayer = pb.buyu.BuYuInvitePlayer(1, 2, 3)
-  val buYuInvitePlayerMessage = WrapperBuyuMessage().withBuYuInvitePlayer(buYuInvitePlayer)
+  def buYuInvite(): Unit = {
+    val buYuInvitePlayer = pb.buyu.BuYuInvitePlayer(1, 2, 3)
+    val buYuInvitePlayerMessage = WrapperBuyuMessage().withBuYuInvitePlayer(buYuInvitePlayer)
+    buYuList = buYuList ++: List(BuYuCass(0, buYuInvitePlayerMessage))
 
+    val buYuInviteConfirm = pb.buyu.BuYuInviteConfirm(2, 1, true)
+    val buYuInviteConfirmMessage = WrapperBuyuMessage().withBuYuInviteConfirm(buYuInviteConfirm)
+    buYuList = buYuList ++: List(BuYuCass(1, buYuInviteConfirmMessage))
+  }
 
+  //MockUser.send(buYuList)
 
+}
+
+class Mock1010()(actorSystem: ActorSystem) (implicit executionContext: ExecutionContext){
+  actorSystem.scheduler.scheduleWithFixedDelay(2.seconds, 2.seconds) { () =>
+
+  }
 }
