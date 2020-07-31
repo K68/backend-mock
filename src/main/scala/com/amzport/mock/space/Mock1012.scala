e4962dbad8127538f8b00a234afa924943885322
Administrator <1161376542@qq.com>
2020-07-31 14:49:17 +0800

    Merge branch 'master' of https://github.com/K68/backend-mock
    # Conflicts:
    #	src/main/scala/com/amzport/mock/MockUser.scala

--- src/main/scala/com/amzport/mock/space/Mock1012.scala
+++ src/main/scala/com/amzport/mock/space/Mock1012.scala
@@ -4,35 +4,67 @@ import com.amzport.mock.{MPB, MockLog, MockUser}
 import com.amzport.mock.MPB.FlowMeta
 import com.google.protobuf.ByteString
 import pb.baccaratwrapper.WrapperBaccaratMessage
-import pb.miracle.dispatch.{DispatchWrapper, RoomDispatch}
-import pb.miracle.dispatch.DispatchWrapper.Msg
+import pb.baccaratwrapper.WrapperBaccaratMessage.Msg
 import scalapb.GeneratedMessage
 
 object Mock1012 {
 
   def sendRoomDispatch(flowMeta: FlowMeta, m: WrapperBaccaratMessage): Unit = {
-    //MockLog.info(s"寻找房间：${m.spaceNew} ${m.meta}", flowMeta.space)
+    MockLog.info(s"进入百家乐：${flowMeta.fromId} ${flowMeta.toId}", flowMeta.space)
     MockUser.send(MPB.toByte(flowMeta, m.toByteString))
     MockLog.debug(s"${m.toString}", 1012)
   }
 
   private def receiveMessage(space: Int, flowMeta: FlowMeta, value: ByteString): Unit = {
-    MPB.parseWrap(DispatchWrapper, value)  match {
+    MPB.parseWrap(WrapperBaccaratMessage, value)  match {
       case Some(wrapper) =>
         wrapper.msg match {
-          case Msg.Result(result) =>
-            if (result.ok) {
+          case Msg.UserSitDownSend(result) =>
               callback(flowMeta, result)
-            } else {
-              MockLog.error("", space)
-            }
-
-          case Msg.IvtRoom(ivt) =>
-            callback(flowMeta, ivt)
-
-          case Msg.LiveRoom(live) =>
-            callback(flowMeta, live)
-
+          case Msg.ApplyBankerSend(result) =>
+            callback(flowMeta, result)
+          case Msg.ChangeBanker(result) =>
+            callback(flowMeta, result)
+          case Msg.PlaceBetSend(result) =>
+            callback(flowMeta, result)
+          case Msg.UserStateChange(result) =>
+            callback(flowMeta, result)
+          case Msg.UsersItem(result)=>
+            callback(flowMeta, result)
+          case Msg.TableCardArrayItem(result)=>
+            callback(flowMeta, result)
+          case Msg.CancelBanker(result)=>
+            callback(flowMeta, result)
+          case Msg.PlaceBetFail(result)=>
+            callback(flowMeta, result)
+          case Msg.PlaceAllBetSend(result)=>
+            callback(flowMeta, result)
+          case Msg.StatusChang(result)=>
+            callback(flowMeta, result)
+          case Msg.GameStart(result)=>
+            callback(flowMeta, result)
+          case Msg.GameEnd(result)=>
+            callback(flowMeta, result)
+          case Msg.TableCardArray(result)=>
+            callback(flowMeta, result)
+          case Msg.PlayScore(result)=>
+            callback(flowMeta, result)
+          case Msg.RecordRoadArrayInt(result)=>
+            callback(flowMeta, result)
+          case Msg.UserItem(result)=>
+            callback(flowMeta, result)
+          case Msg.RecordRoadArrayString(result)=>
+            callback(flowMeta, result)
+          case Msg.RecordRoadString(result)=>
+            callback(flowMeta, result)
+          case Msg.RecordRoadInt(result)=>
+            callback(flowMeta, result)
+          case Msg.GameRecordSend(result)=>
+            callback(flowMeta, result)
+          case Msg.UserStateChange(result)=>
+            callback(flowMeta, result)
+          case Msg.TipsMessage(result)=>
+            callback(flowMeta, result)
           case _ =>
             MockLog.warn("useless dispatch message", space)
         }
--- src/main/scala/com/amzport/mock/test/TestZYC.scala
+++ src/main/scala/com/amzport/mock/test/TestZYC.scala
@@ -1,15 +1,22 @@
 package com.amzport.mock.test
 
+import java.util
+
 import com.amzport.mock.MPB.FlowMeta
+import com.amzport.mock.MockSystem.system
 import com.amzport.mock.space.{Mock1000, Mock1012}
+import com.amzport.mock.test.Test.outActor
 import com.amzport.mock.{MPB, MockLog, MockUser, MockWeb}
 import pb.baccarat._
 import pb.baccaratwrapper.WrapperBaccaratMessage
 import pb.inoutroomwrapper.WrapperInOutRoomMessage
 
+import scala.concurrent.duration._
+
 object TestZYC extends App {
 
-  MockUser.setupMockUser("111", "111", "http://127.0.0.1:9002/api/auth/login", "ws://127.0.0.1:9002/wsz")
+  var IsInto = false
+  MockUser.setupMockUser("116", "111", "http://127.0.0.1:9002/api/auth/login", "ws://127.0.0.1:9002/wsz")
   MockUser.observe(999, (space, meta, value) => {
     // TODO
 
@@ -18,8 +25,65 @@ object TestZYC extends App {
   // MockSpaceXX 的参考用法
   Mock1012.observe((flowMeta, msg) => {
     msg match {
-      case x: UserSitDownReceive =>
-      case x: UserStandUp =>
+      case x: UserSitDownSend =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: ApplyBankerSend =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: ChangeBanker =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: PlaceBetSend =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: UserStateChange =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x:UsersItem =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x:UserItem=>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: TableCardArrayItem =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: CancelBanker =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: PlaceBetFail =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: PlaceAllBetSend =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: StatusChang =>
+        MockLog.debug(s"${x.toString}", 1012)
+        MockLog.debug(s"${IsInto.toString}", 1012)
+        MockLog.debug(s"${x.status.toString}", 1012)
+        if(IsInto && x.status == 2)
+        {
+          //system.scheduler.scheduleOnce(7.seconds) {
+          MockLog.debug("PlaceBet", 1012)
+          val ran1 = (new util.Random).nextInt(32767)
+          val BetArea =ran1 % (7)
+          val PlaceBetReceive = WrapperBaccaratMessage().withPlaceBetReceive(pb.baccarat.PlaceBetReceive(0L,BetArea,500L,1))
+          Mock1012.sendRoomDispatch(FlowMeta(1012, "6", 1, "32", 2), PlaceBetReceive)
+          //}
+
+        }
+      case x:GameStart =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x:GameEnd=>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: TableCardArray =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: PlayScore =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: RecordRoadArrayInt =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: RecordRoadInt =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x: GameRecordSend =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x:RecordRoadArrayString =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x:RecordRoadString=>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x:UserStateChange =>
+        MockLog.debug(s"${x.toString}", 1012)
+      case x:TipsMessage=>
+        MockLog.debug(s"${x.toString}", 1012)
     }
   })
 
@@ -30,15 +94,39 @@ object TestZYC extends App {
     m("msg") match {
       //用户进入百家乐
       case "UserSitDownReceive" =>  //URL: http://127.0.0.1:9999/1012?msg=UserSitDownReceive&userId=?&roomId=?
-        println(m)
         val userId = m("userId")
         val roomId = m("roomId")
+        MockLog.debug(s"${userId.toString}", 1012)
+        if(userId.toInt == 6) {
+          IsInto = true
+          MockLog.debug(s"${IsInto.toString}", 1012)
+        }
         val UserSitDownReceive = WrapperBaccaratMessage().withUserSitDownReceive(pb.baccarat.UserSitDownReceive(userId.toLong, roomId.toInt))
         Mock1012.sendRoomDispatch(FlowMeta(1012, userId, 1, roomId, 2), UserSitDownReceive)
-        //MockUser.send(MPB.toByte(FlowMeta(1012, userId, 1, roomId, 2), UserSitDownReceive))
-      //用户进入百家乐
+      //用户申请庄家
+      case "ApplyBankerReceive" =>  //URL: http://127.0.0.1:9999/1012?msg=ApplyBankerReceive&userId=?&roomId=?
+        val userId = m("userId")
+        val roomId = m("roomId")
+        val ApplyBankerReceive = WrapperBaccaratMessage().withApplyBankerReceive(pb.baccarat.ApplyBankerReceive(userId.toLong))
+        Mock1012.sendRoomDispatch(FlowMeta(1012, userId, 1, roomId, 2), ApplyBankerReceive)
+      //用户下庄
+      case "CancelBanker" =>  //URL: http://127.0.0.1:9999/1012?msg=ApplyBankerReceive&userId=?&roomId=?&ChairID=?&ListApplyCount=?
+        val userId = m("userId")
+        val roomId = m("roomId")
+        val CancelBanker = WrapperBaccaratMessage().withCancelBanker(pb.baccarat.CancelBanker(userId.toLong))
+        Mock1012.sendRoomDispatch(FlowMeta(1012, userId, 1, roomId, 2), CancelBanker)
+      //用户下注
+      case "PlaceBetReceive" =>  //URL: http://127.0.0.1:9999/1012?msg=PlaceBetReceive&userId=?&roomId=?&ChairID=?&BetArea=?&BetScore=?&AndroidUser=?
+        val userId = m("userId")
+        val roomId = m("roomId")
+        val ChairID = m("ChairID")
+        val BetArea = m("BetArea")
+        val BetScore = m("BetScore")
+        val AndroidUser = m("AndroidUser")
+        val PlaceBetReceive = WrapperBaccaratMessage().withPlaceBetReceive(pb.baccarat.PlaceBetReceive(ChairID.toLong,BetArea.toInt,BetScore.toLong,AndroidUser.toInt))
+        Mock1012.sendRoomDispatch(FlowMeta(1012, userId, 1, roomId, 2), PlaceBetReceive)
+      //用户退出百家乐
       case "UserStandUp" =>  //URL: http://127.0.0.1:9999/1012?msg=UserStandUp&userId=?&roomId=?&ChairID=?
-        println(m)
         val ChairID = m("ChairID")
         val userId = m("userId")
         val roomId = m("roomId")
